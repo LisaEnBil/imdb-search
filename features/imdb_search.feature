8e2a9f93325c4b4e0525a41616083e744af0d420
Lisa Gillfrost <smartalisa@hotmail.com>
2020-01-20 13:34:13 +0100

    Mer kod som inte fungerar

--- features/imdb_search.feature
+++ features/imdb_search.feature
@@ -1,17 +1,17 @@
 
 Feature: IMDB-search
+  
+   Scenario Outline: Search for a movie using <actors_name>
+   Given that I am on the IMDB website
+   When I enter <actors_name> in a search field
+   And I click the search button
+   Then I find the corresponding <actors_name> in the search results
 
-As a movie-buff 
-I'd like to search for movies
+   Examples:
+   | actors_name        | 
+   | "Chevy Chase"      |
+   | "Colin Firth"      | 
 
+   
+  
 
-Scenario Outline: Search for a movie
-Given that I am on the IMDB website
-When I enter the <movie_name> a search field
-And I click the search button
-Then I find the corresponding <movie_name> in the search results
-
-Examples:
-| movie_name    | 
-| "Antz"        |
-| "A bug's life"|
--- step-definitions/imdb_search.js
+++ step-definitions/imdb_search.js
@@ -1,31 +1,49 @@
 
-let {$, sleep} = require('./funcs');
+let { $, sleep } = require('./funcs');
 
-module.exports = function() {
+let normalSleep = 1000;
 
-    this.Given(/^that I am on the IMDB website$/, async function () {
-       await helpers.loadPage('https://imdb.com');
-       await sleep(1000);
-       let logo = await $('#home_img_holder');
-       assert(logo, "Expected the IMDB logo");
-    });
+module.exports = function () {
 
+  this.Given(/^that I am on the IMDB website$/, async function () {
+    await helpers.loadPage('https://imdb.com');
+    await sleep(normalSleep);
+    let logo = await $('#home_img_holder');
+    assert(logo, "Expected the IMDB logo");
+  });
 
-    this.When(/^I enter the "([^"]*)" a search field$/, async function (movieTitle) {
-       
-      let searchField = await $('#suggestion-search');
-      await searchField.sendKeys(movieTitle);
-      await sleep(5000);
+  this.When(/^I enter "([^"]*)" in a search field$/, async function (actorsName) {
 
-    });
+    let searchField = await $('#suggestion-search');
+    await searchField.sendKeys(actorsName);
+    await sleep(normalSleep);
+  });
 
-    this.When(/^I click the search button$/, function () {
-       
-     });
+  this.When(/^I click the search button$/, async function () {
 
-    this.Then(/^I find the corresponding "([^"]*)" in the search results$/, function () {
-       
-      });
+    let searchButton = await $('#suggestion-search-button');
+    await searchButton.click();
+    await sleep(normalSleep);
 
+  });
+
+  this.Then(/^I find the corresponding "([^"]*)" in the search results$/, async function (searchString) {
+
+    let results = await $('div:nth-child(3) > table > tbody > tr > td.result_text');
+
+    let found = false;
+    //console.log("hitta koden", result);
+    for (let result of results) {
+      console.log("TEXT", result.getText());
+      let text = await result.getText();
+
+      if (text.toLowerCase().includes(searchString.toLowerCase())) {
+        found = true;
+        break;
+      }
+    }
+    assert(found, 'expected to find ' + searchString)
+
+  });
 
 }
